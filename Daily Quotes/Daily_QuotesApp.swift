6610e720dbf313e43992a6f09df13e7f153318f4
Anastasio Almansa <at.almansa@gmail.com>
2023-05-01 16:54:40 +0100

    Initial Commit

---
+++ Daily Quotes/Daily_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_QuotesApp.swift
+//  Daily Quotes
+//
+//  Created by Anastasio Del Toro Almansa on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Daily_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
